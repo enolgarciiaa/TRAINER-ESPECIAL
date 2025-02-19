87ebee97367eb7ab9b1cdb4fa63750510951aeaa
Enol Garcia Heres <enolgarciiaa@gmail.com>
2025-02-19 15:09:10 +0100

    cambios finales

--- JS/galeria.js
+++ JS/galeria.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/json/galeria.json")
+    fetch("https://enolgarciiaa.github.io/TRAINER-ESPECIAL/json/galeria.json")
         .then(response => response.json())
         .then(data => {
             let galeria = document.getElementById("galeria");
